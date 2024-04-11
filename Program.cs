fb1b0da00f31921a91e0b47c56e6a81c169f71ce
luiarhs <luiarhs@yandex.com>
2024-04-10 23:21:31 -0600

    [add] health check for the service

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<StartupBackgroundService>();
+builder.Services.AddSingleton<StartupHealthCheck>();
+
+builder.Services.AddHealthChecks()
+    .AddCheck<StartupHealthCheck>(
+        "Startup",
+        tags: new[] { "ready" });
 
 var app = builder.Build();
 
@@ -21,5 +30,17 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/healthz");
+
+app.MapHealthChecks("/healthz/ready", new HealthCheckOptions
+{
+    Predicate = healthCheck => healthCheck.Tags.Contains("ready")
+});
+
+app.MapHealthChecks("/healthz/live", new HealthCheckOptions
+{
+    Predicate = _ => false
+});
 
 app.Run();
